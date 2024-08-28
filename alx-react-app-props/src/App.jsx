58fdeef22ab97afb73ecdb6cb0df4c767f2faa05
unknown <emmanuelpeculiar645@gmail.com>
2024-08-28 12:25:47 +0100

    third commit

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component} from 'react';
+import './App.css';
+import UserDetails from './Components/UserDetails';
+import { UserContext } from './Components/UserContext';
+
+
+class App extends Component {
+  render() {
+     const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+    return (
+      <div className='App'>
+        <UserContext.Provider  value={ {userData} }>
+            <UserDetails />
+        </UserContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ alx-react-app-props/src/Components/UserDetails.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
+
+const UserDetails = () => {
+  const { userData } = useContext(UserContext);
+
+  return (
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+      <h2 style={{ color: 'blue' }}>Name: {userData.name}</h2>
+      <p>Email: <span style={{ fontWeight: 'bold' }}>{userData.email}</span></p>
+    </div>
+  );
+};
+
+export default UserDetails;
