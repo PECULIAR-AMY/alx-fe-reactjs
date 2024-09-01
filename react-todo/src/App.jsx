022818787f084feadfe12f15af36f631e15ac18d
unknown <emmanuelpeculiar645@gmail.com>
2024-09-01 12:36:27 +0100

    newest commit

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+	return (
+		<>
+			<h1>React Todo</h1>
+			<TodoList />
+		</>
+	);
+}
+
+export default App;
