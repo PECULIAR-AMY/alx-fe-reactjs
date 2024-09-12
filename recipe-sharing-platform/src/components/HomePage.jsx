0c0e3c74f61560afb1c52c852c61b9aa552ba3e2
unknown <emmanuelpeculiar645@gmail.com>
2024-09-12 15:00:37 +0100

    new commit

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import data from '../data.json';
+import { Link } from 'react-router-dom';
 
 export const HomePage = () => {
 	const [recipes, setRecipes] = useState(data);
